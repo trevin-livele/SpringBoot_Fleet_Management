db20675f75a9678296acd8794cefc660c4f580b6
trevin-livele <liveletrevin6@gmail.com>
2022-12-08 11:14:00 -0500

    added models

---
+++ src/main/java/com/trevin/FleetMangmt/models/User.java
@@ -0,0 +1,26 @@
+package com.trevin.FleetMangmt.models;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class User {
+		
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+	private int id;
+	private String username;
+	private String password;
+}
---
+++ src/main/java/com/trevin/FleetMangmt/models/VehicleMake.java
@@ -0,0 +1,17 @@
+package com.trevin.FleetMangmt.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper=false)
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class VehicleMake extends CommonObject {
+
+}
